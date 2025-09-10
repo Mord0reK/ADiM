30666c73ddf253a995712d44b5db156dc4e0b167
Mord0reK <marcel.stosio51@gmail.com>
2025-09-10 12:52:59 +0200

    Lekcja 1

---
+++ Lekcja 1 - JavaScript/zadanie_2.js
@@ -0,0 +1,8 @@
+var imie = prompt("Podaj swoje Imię: ");
+var nazwisko = prompt("Podaj swoje Nazwisko: ");
+
+document.write("<a> Imię: </a>")
+document.write("<a style='color: blue;'>" +imie+ "</a>")
+document.write("<br>")
+document.write("<a> Nazwisko: </a>")
+document.write("<a style='color: red;'>" +nazwisko+ "</a>")
